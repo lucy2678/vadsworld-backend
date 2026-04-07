09b6ee650eb09bbc8a364874d54c326af6f17367
lucy2678 <bylucy2020@gmail.com>
2026-04-07 11:20:15 +0200

    Final database and CORS setup

--- main.py
+++ main.py
@@ -21,8 +21,18 @@ app.add_middleware(
 )
 
 # Database Setup
-SQLALCHEMY_DATABASE_URL = "sqlite:///./vadsworld.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./vadsworld.db")
+
+# Fix for newer SQLAlchemy versions that require 'postgresql://' instead of 'postgres://'
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# SQLite requires check_same_thread=False, but PostgreSQL doesn't support it
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
